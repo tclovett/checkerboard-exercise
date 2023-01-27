915d0aac816125d3d57b65aa233c651dca22f328
Taylor Lovett <tclovett7@gmail.com>
2023-01-27 17:47:31 -0600

    v1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let oneColor = 255;
+let otherColor = 145;
 for(let i = 0; i < (9*9); i++) {
     var tile = document.createElement("div");
     tile.classList.add('tile');
@@ -5,10 +7,12 @@ for(let i = 0; i < (9*9); i++) {
     tile.style.minWidth = "11.1%";
     tile.style.paddingBottom = '11.1%';
     if (i % 2 === 0) {
-        tile.style.backgroundColor = "black";
+        tile.style.backgroundColor = "rgba("+0+","+oneColor+","+oneColor+")"; 
     }
     else {
-        tile.style.backgroundColor = 'red';
+        tile.style.backgroundColor = "rgba("+oneColor+","+otherColor+","+0+")";
     }
+    otherColor += 2;
+    oneColor -= 2;
     document.body.appendChild(tile);
 }
