9c10001fe66f3ae511aa8ae680a568def4e35069
Taylor Lovett <tclovett7@gmail.com>
2023-01-27 06:56:52 -0600

    finished checkerboard exercise

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+for(let i = 0; i < (9*9); i++) {
+    var tile = document.createElement("div");
+    tile.classList.add('tile');
+    tile.style.float = 'left';
+    tile.style.minWidth = "11.1%";
+    tile.style.paddingBottom = '11.1%';
+    if (i % 2 === 0) {
+        tile.style.backgroundColor = "black";
+    }
+    else {
+        tile.style.backgroundColor = 'red';
+    }
+    document.body.appendChild(tile);
+}
