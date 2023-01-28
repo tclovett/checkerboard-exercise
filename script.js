863b509361bf77edeb582ee520c18b2cdfe74f52
Taylor Lovett <tclovett7@gmail.com>
2023-01-27 18:16:54 -0600

    v1

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-let oneColor = 255;
-let otherColor = 145;
+function randomColor(){
+    let colorToUse = Math.floor(Math.random() * 255);
+    return colorToUse;
+}
+function createNewBoard() {
+        for (i = 4; i < document.body.childNodes.length; i++) {
+            document.body.childNodes[i].style.backgroundColor = 'rgba(' 
+            + randomColor() + ',' + randomColor() 
+            + ',' + randomColor() + '\)';
+        }
+        
+}
+
 for(let i = 0; i < (9*9); i++) {
     var tile = document.createElement("div");
     tile.classList.add('tile');
     tile.style.float = 'left';
     tile.style.minWidth = "11.1%";
     tile.style.paddingBottom = '11.1%';
-    if (i % 2 === 0) {
-        tile.style.backgroundColor = "rgba("+0+","+oneColor+","+oneColor+")"; 
-    }
-    else {
-        tile.style.backgroundColor = "rgba("+oneColor+","+otherColor+","+0+")";
-    }
-    otherColor += 2;
-    oneColor -= 2;
+    tile.style.backgroundColor = 'rgba(' 
+    + randomColor() + ',' + randomColor() 
+    + ',' + randomColor() + '\)';
     document.body.appendChild(tile);
 }
+
+setInterval(createNewBoard,2000);
